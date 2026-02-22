1c7957bbbd74c3fd5920286a9a1ef67ea81359bb
Ansder <60071687+Ansder@users.noreply.github.com>
2026-02-22 07:37:26 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,1 +1,38 @@
 
+function templateModern(data){
+  return `
+    <div class="template-modern">
+      <div class="left">
+        <h2>${data.name}</h2>
+        <p>${data.title}</p>
+      </div>
+      <div class="right">
+        <h3>Profil</h3>
+        <p>${data.profile}</p>
+        <h3>Experiență</h3>
+        <p>${data.experience}</p>
+        <h3>Educație</h3>
+        <p>${data.education}</p>
+        <h3>Skill-uri</h3>
+        <p>${data.skills}</p>
+      </div>
+    </div>
+  `;
+}
+
+function templateClean(data){
+  return `
+    <div class="template-clean">
+      <h2>${data.name}</h2>
+      <p><strong>${data.title}</strong></p>
+      <h3>Profil</h3>
+      <p>${data.profile}</p>
+      <h3>Experiență</h3>
+      <p>${data.experience}</p>
+      <h3>Educație</h3>
+      <p>${data.education}</p>
+      <h3>Skill-uri</h3>
+      <p>${data.skills}</p>
+    </div>
+  `;
+}
